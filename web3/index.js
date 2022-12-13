3311ece062010bd4b2b40a21f34f4a12bac20265
nnph <adilzhanov@aol.com>
2022-12-13 13:36:56 +0300

    test commit

---
+++ web3/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { useWeb3 } from '@metamask/react-web3';
+import { Binance } from '@binance-chain/javascript-sdk';
+
+const App = () => {
+  const [account, setAccount] = useState(null);
+  const web3 = useWeb3();
+
+  const handleConnect = async () => {
+    if (!web3) return;
+
+    // Set the Binance network to Binance Smart Chain
+    Binance.setNetwork('mainnet');
+
+    // Get the user's Binance account address
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+  };
+
+  return (
+    <div>
+      {!account && (
+        <button onClick={handleConnect}>Connect with MetaMask</button>
+      )}
+      {account && <p>Your Binance account address: {account}</p>}
+    </div>
+  );
+};
+
+export default App;
